ffc553b8cc08910de105daf3815e716b4e46cf2e
F <filip.prvulj@gmail.com>
2021-03-12 19:19:28 +0100

    Add base classes

---
+++ eight-ball/EightBall.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using EightBall.Data.Entities;
+using EightBall.Shared.Dtos;
+using EightBall.Shared.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightBall.Data.Repositories
+{
+    public class BaseRepository<TEntity, TDto> : IBaseRepository<TDto>
+        where TEntity : BaseEntity
+        where TDto : BaseDto
+    {
+        public Task<Guid> AddEntityAsync(TDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TDto>> GetEntitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TDto> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> RemoveEntityAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateEntityAsync(TDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ eight-ball/EightBall.Service/Services/BaseService.cs
@@ -0,0 +1,39 @@
+﻿using EightBall.Shared.Dtos;
+using EightBall.Shared.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightBall.Service.Services
+{
+    public class BaseService<TDto> : IBaseService<TDto>
+        where TDto : BaseDto
+    {
+        public Task<TDto> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TDto>> GetEntitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> InsertAsync(TDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> RemoveAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(TDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
