542ce9c46d329a79bf2c8c424b9e31dafab119fa
F <filip.prvulj@gmail.com>
2021-03-13 15:40:00 +0100

    Change project template

---
+++ eight-ball/EightBall.MVC/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EightBall.MVC.Mapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+        }
+    }
+}
